56003caf3c543765986500093d4c2fd5361ef3d1
watanaberyunosuke <harrybwatson@protonmail.com>
2023-02-15 22:27:48 +1100

    Add basic state colab

---
+++ BasicStateCodeLab/app/src/main/java/com/mysterioussoftware/basicstatecodelab/WellnessTask.kt
@@ -0,0 +1,9 @@
+package com.mysterioussoftware.basicstatecodelab
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+data class WellnessTask(
+    val id: Int,
+    val label: String,
+    var checked: MutableState<Boolean> = mutableStateOf(false))
