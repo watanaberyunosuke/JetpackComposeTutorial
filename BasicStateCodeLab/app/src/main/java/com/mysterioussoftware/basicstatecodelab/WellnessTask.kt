da543164eca2297fd25053d2f43f16c92446a6ea
watanaberyunosuke <harrybwatson@protonmail.com>
2023-02-15 22:35:38 +1100

    fix basic states colab bug

--- BasicStateCodeLab/app/src/main/java/com/mysterioussoftware/basicstatecodelab/WellnessTask.kt
+++ BasicStateCodeLab/app/src/main/java/com/mysterioussoftware/basicstatecodelab/WellnessTask.kt
@@ -1,9 +1,12 @@
 package com.mysterioussoftware.basicstatecodelab
 
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-data class WellnessTask(
+class WellnessTask(
     val id: Int,
     val label: String,
-    var checked: MutableState<Boolean> = mutableStateOf(false))
+    initialChecked: Boolean = false) {
+    var checked: Boolean by mutableStateOf(initialChecked)
+}
